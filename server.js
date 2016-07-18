50c54d454ba2ec57cae5094ac90e8cd3ec718a23
Assassin kin <iuns@outlook.fr>
2016-07-18 16:12:32 +0100

    Refactor middleware

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 var express = require('express');
 var app = express();
 var PORT = 3000; // if the variable name is in uppercase it means its a constant and we musnt change it
-var middleware = {
-	requireAuthentication: function (req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req,res,next){
-		console.log('Request '+ new Date().toString() +' '+req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
-//app.use(middleware.requireAuthentication);
+var middleware = require("./middleware.js")
 app.use(middleware.logger);
 
+//app.use(middleware.requireAuthentication);
 //app.get('/', function (req , res){
 //	res.send('hello express!');
 //});
+
 
 app.get('/about', middleware.requireAuthentication, function (req , res){
 	res.send('hello express about ? ');
