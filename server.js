584968af85271e442b242839a289ff11694099a2
Assassin kin <iuns@outlook.fr>
2016-07-19 10:38:03 +0100

    Update port for heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var PORT = 3000; // if the variable name is in uppercase it means its a constant and we musnt change it
+var PORT = process.env.PORT || 3000; // if the variable name is in uppercase it means its a constant and we musnt change it
 var middleware = require("./middleware.js")
 app.use(middleware.logger);
 
 //app.use(middleware.requireAuthentication);
 //app.get('/', function (req , res){
-//	res.send('hello express!');
+//	res.send('hello express!cd ');
 //});
 
 
